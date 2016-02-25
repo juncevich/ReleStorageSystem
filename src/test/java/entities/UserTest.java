b49bc89cac03e7c8b0c141fc9cda9520c3b49435
Alex <a.juncevich@gmail.com>
2016-02-25 15:28:20 +0500

    Add first test.

--- src/test/java/entities/UserTest.java
+++ src/test/java/entities/UserTest.java
@@ -1,6 +1,33 @@
 package entities;
 
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserTest {
+
+    User userWithEmptyConstructor = new User();
+    User getUserWithNotEmptyConstructor = new User("TestUser", "TestPassword", "TestGroup");
+    @Test
+    public void testEmptyConstructor() throws Exception{
+
+        String nullName = null;
+        String nullGroup = null;
+        String nullPassword = null;
+        Assert.assertThat(userWithEmptyConstructor.getName(), CoreMatchers.is(nullName));
+        Assert.assertThat(userWithEmptyConstructor.getGroup(), CoreMatchers.is(nullGroup));
+        Assert.assertThat(userWithEmptyConstructor.getPassword(), CoreMatchers.is(nullPassword));
+    }
+    @Test
+    public void testNotEmptyConstructor() throws Exception{
+
+        String Name = "TestUser";
+        String Group = "TestGroup";
+        String Password = "TestPassword";
+        Assert.assertThat(getUserWithNotEmptyConstructor.getName(), CoreMatchers.is(Name));
+        Assert.assertThat(getUserWithNotEmptyConstructor.getGroup(), CoreMatchers.is(Group));
+        Assert.assertThat(getUserWithNotEmptyConstructor.getPassword(), CoreMatchers.is(Password));
+    }
 
 }
