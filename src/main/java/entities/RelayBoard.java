50e78e7cf68e71036906aa469691df94c638dc00
Alex <a.juncevich@gmail.com>
2016-02-25 14:39:23 +0500

    Add some classes.

---
+++ src/main/java/entities/RelayBoard.java
@@ -0,0 +1,11 @@
+package entities;
+
+import interfaces.Location;
+
+/**
+ * Relay board may can be at any railway crossing,
+ * any railway point and any station. It has six shelves.
+ * Each shelve has maximum eight connectors to connect the relay.
+ */
+public class RelayBoard implements Location {
+}
---
+++ src/main/java/entities/Shelve.java
@@ -0,0 +1,11 @@
+package entities;
+
+
+import interfaces.Location;
+
+/**
+ * Statives and relay boards have a shelves.
+ * Max shelve capasity is eight.
+ */
+public class Shelve implements Location{
+}
