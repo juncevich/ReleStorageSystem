134621f7eb15480ff4512046e280a423274c6821
alex <a.juncevich@gmail.com>
2016-07-28 23:15:18 +0500

    Tune shelve entity

--- src/main/java/ru/relesystem/entities/location/Shelve.java
+++ src/main/java/ru/relesystem/entities/location/Shelve.java
@@ -1,7 +1,13 @@
 package ru.relesystem.entities.location;
 
+import ru.relesystem.entities.Relay;
+
 /**
  * Created by alex on 28.07.16.
  */
 public class Shelve {
+    private Long id;
+    private Byte number;
+    private Stativ stativ;
+    private Relay[] relayArray;
 }
