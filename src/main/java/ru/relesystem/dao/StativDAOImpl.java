0db48889ba5053e520c16989f8383dc821df3109
alex <a.juncevich@gmail.com>
2016-08-04 21:00:35 +0500

    Add Stativ DAO implementation

---
+++ src/main/java/ru/relesystem/dao/StativDAOImpl.java
@@ -0,0 +1,25 @@
+package ru.relesystem.dao;
+
+import ru.relesystem.entities.storage.Stativ;
+
+import java.util.List;
+
+/**
+ * Created by alex on 04.08.16.
+ */
+public class StativDAOImpl implements StativDAO {
+    @Override
+    public List<Stativ> getStatives() {
+        return null;
+    }
+
+    @Override
+    public Stativ getStativById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Stativ save(Stativ stativ) {
+        return null;
+    }
+}
