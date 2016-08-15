a714acc25bd353512379da1317e606eb126d5ea2
alex <a.juncevich@gmail.com>
2016-08-16 01:24:19 +0500

    dfgdg

--- src/main/java/ru/relesystem/controllers/JSTLController.java
+++ src/main/java/ru/relesystem/controllers/JSTLController.java
@@ -6,28 +6,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import ru.relesystem.entities.Relay;
 import ru.relesystem.services.RelayService;
+
+import java.util.List;
 
 
 @Controller
 public class JSTLController {
 
-    @Autowired
+
     private RelayService relayService;
 
     @RequestMapping(value = "/jstlReturnRelays", method = RequestMethod.GET)
     public ModelAndView listRelays() {
-        return new ModelAndView("jstl/jstl", "resultObjectList", relayService.findAll());
+        List<Relay> relayList = relayService.findAll();
+        return new ModelAndView("jstl/jstl", "jstlObjectList", relayList);
     }
 
     @RequestMapping(value="/jstlRelay", method=RequestMethod.GET)
     public ModelAndView getRelay() {;
-        return new ModelAndView("jstl/jstl", "resultObject", relayService.findById(1L));
+        return new ModelAndView("jstl/jstl", "jstlObject", relayService.findById(1L));
     }
 
     @RequestMapping(value="/jstlHTML", method=RequestMethod.GET)
     public ModelAndView returnHTML() {
-        return new ModelAndView("jstl/jstl", "resultHTML", "<font color='red'><b>Test Color Red</b></font>");
+        return new ModelAndView("jstl/jstl", "jstlHTML", "<font color='red'><b>Test Color Red</b></font>");
+    }
+
+    @Qualifier("jpaRelayService")
+    @Autowired
+    public void setRelayService( RelayService relayService) {
+        this.relayService = relayService;
     }
 
 }
