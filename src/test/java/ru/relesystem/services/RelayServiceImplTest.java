624a4ac9072453005468aaf35d1dafb23fbe51ac
alex <a.juncevich@gmail.com>
2016-09-15 00:07:25 +0500

    Add test to RelayService

---
+++ src/test/java/ru/relesystem/services/RelayServiceImplTest.java
@@ -0,0 +1,40 @@
+package ru.relesystem.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by alex on 15.09.16.
+ */
+public class RelayServiceImplTest {
+
+    @Before public void setUp() throws Exception {
+
+    }
+
+    @After public void tearDown() throws Exception {
+
+    }
+
+    @Test public void findAll() throws Exception {
+
+    }
+
+    @Test public void findAllWithDetail() throws Exception {
+
+    }
+
+    @Test public void findById() throws Exception {
+
+    }
+
+    @Test public void save() throws Exception {
+
+    }
+
+    @Test public void delete() throws Exception {
+
+    }
+
+}
