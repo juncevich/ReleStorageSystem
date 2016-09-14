3c85d7f03bb51263db0da2323af73c9070e0b610
alex <a.juncevich@gmail.com>
2016-09-14 22:24:25 +0500

    Add gitignore

--- src/main/java/ru/relesystem/excelpdf/Cat.java
+++ src/main/java/ru/relesystem/excelpdf/Cat.java
@@ -1,40 +1,38 @@
 package ru.relesystem.excelpdf;
 
-
 class Cat {
 
+	private String name;
+	private String color;
+	private int weight;
 
-    private String name;
-    private String color;
-    private int weight;
+	public Cat(String name, String color, int weight) {
+		this.name = name;
+		this.color = color;
+		this.weight = weight;
+	}
 
-    public Cat(String name, String color, int weight) {
-        this.name = name;
-        this.color = color;
-        this.weight = weight;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public void setColor(String color) {
+		this.color = color;
+	}
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+	public int getWeight() {
+		return weight;
+	}
 
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
 }
