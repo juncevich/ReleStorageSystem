3909d745f2d8f315406f8b3d81f701667cc38023
alex <a.juncevich@gmail.com>
2016-09-18 23:21:35 +0500

    Try add hbm2ddl plugin

--- src/main/java/ru/relesystem/entities/person/Worker.java
+++ src/main/java/ru/relesystem/entities/person/Worker.java
@@ -4,6 +4,8 @@ import org.joda.time.DateTime;
 
 class Worker extends Person {
 
+	private static final long serialVersionUID = 1931740563334569696L;
+
 	private DateTime startWorking;
 	private DateTime endWorking;
 
