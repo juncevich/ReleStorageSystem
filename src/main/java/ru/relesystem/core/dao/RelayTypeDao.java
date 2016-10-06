f1d2d6ed9445b507adf288e4392d7c9b4953926e
alex <a.juncevich@gmail.com>
2016-10-06 21:01:13 +0500

    Add DAO interface RelayTypeDao.

--- src/main/java/ru/relesystem/core/dao/RelayTypeDao.java
+++ src/main/java/ru/relesystem/core/dao/RelayTypeDao.java
@@ -1,5 +1,14 @@
 package ru.relesystem.core.dao;
 
+import java.util.List;
+
+import ru.relesystem.core.entities.relaytype.RelayType;
+
 public interface RelayTypeDao {
 
+	List<RelayType> findAll();
+	RelayType findById(Long id);
+	RelayType findByName(String name);
+	RelayType updateType(RelayType relayType);
+	void deleteType(RelayType relayType);
 }
