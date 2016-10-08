a824d0545689cb95b04d4c55ff10b0983ca20faa
alex <a.juncevich@gmail.com>
2016-10-08 11:13:18 +0500

    Writing test for RelayTypeService DAO.

---
+++ src/test/java/ru/relesystem/core/dao/RelayTypeDaoTest.java
@@ -0,0 +1,56 @@
+package ru.relesystem.core.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by alex on 08.10.16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:application-context-test.xml")
+public class RelayTypeDaoTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void findAll() throws Exception {
+
+    }
+
+    @Test
+    public void findById() throws Exception {
+
+    }
+
+    @Test
+    public void findByName() throws Exception {
+
+    }
+
+    @Test
+    public void addType() throws Exception {
+
+    }
+
+    @Test
+    public void updateType() throws Exception {
+
+    }
+
+    @Test
+    public void deleteType() throws Exception {
+
+    }
+
+}
