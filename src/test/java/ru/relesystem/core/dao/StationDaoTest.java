6af08ddd502921fa4d2081b2940fc97be442c9c0
alex <a.juncevich@gmail.com>
2016-10-17 20:58:54 +0500

    Add StationDaoTest

---
+++ src/test/java/ru/relesystem/core/dao/StationDaoTest.java
@@ -0,0 +1,43 @@
+package ru.relesystem.core.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by alex on 17.10.16.
+ */
+public class StationDaoTest {
+
+    @Before public void setUp() throws Exception {
+
+    }
+
+    @Test public void findAll() throws Exception {
+
+    }
+
+    @Test public void findLocationByName() throws Exception {
+
+    }
+
+    @Test public void findRelayByLocationName() throws Exception {
+
+    }
+
+    @Test public void findStoragesByLocationName() throws Exception {
+
+    }
+
+    @Test public void addLocation() throws Exception {
+
+    }
+
+    @Test public void updateLocation() throws Exception {
+
+    }
+
+    @Test public void deleteLocation() throws Exception {
+
+    }
+
+}
